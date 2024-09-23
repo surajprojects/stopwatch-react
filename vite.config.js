d176b36a1bcb5f5453f734588f2db6606f133f2a
TIGER <suraj23082002@gmail.com>
2024-09-23 17:23:33 +0530

    configured vite config js file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/stopwatch-react/',  // This should match your GitHub repo name
   plugins: [react()],
 })
+
+
